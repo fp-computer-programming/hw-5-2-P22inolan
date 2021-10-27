f743c9238dadca5aef0675b09b826a3ab9f34897
P22inolan <p22inolan@fairfieldprep.org>
2021-10-27 09:46:43 -0400

    Submit 5-2

---
+++ hw5-2.py
@@ -0,0 +1,23 @@
+# Author: IBN (AMDG) 10/26/2021
+import math
+
+# Question 1
+print(529 ** .5)
+# Question 2
+print(math.sin(0))
+# Question 3
+print(math.cos(2 * math.pi))
+# Question 4
+print(2.5 ** 5)
+# Question 5
+print(math.log(math.e))
+# Question 6
+print(math.e ** 4)
+# Question 7
+print(math.ceil(-12.9))
+# Question 8
+print(math.floor(7.25))
+# Question 9
+print((math.log(1000) * 2))
+# Question 10
+print(math.radians(180))
